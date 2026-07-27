9beed067b37752e9ae26a5008ecf1a0d2d668a62
Gustavo Montenari Pechta <mrpecz@proton.me>
2026-07-27 09:05:01 -0300

    wupper_module: Integrate with libvfio

--- wupper_module/wupper.cpp
+++ wupper_module/wupper.cpp
@@ -30,28 +30,38 @@
  ******************************************************************/
 
 #include "wupper.h"
+#include <sys/mman.h>
 
 // Constants
 #define MAX_TLP_BYTES 256
 
-Wupper::Wupper() {}
-
-void Wupper::dma_to_host(size_t transfer_size, uint64_t start, uint8_t dma_index)
+Wupper::Wupper(const std::string device_name, int iommu_group_id)
+    : bar0(DataBuffer())
+    , interface(VFIO(device_name, iommu_group_id))
 {
+    interface.bar_map_buffer(VFIO_PCI_BAR0_REGION_INDEX, bar0);
+}
+
+void Wupper::dma_to_host(size_t transfer_size, uint8_t dma_index, DataBuffer &buffer)
+{
+    volatile wuppercard_bar0_regs_t* bar0_regs = (volatile wuppercard_bar0_regs_t *)bar0.data;
+
     // Create and enable DMA descriptor (Wupper)
-    volatile dma_descriptor_t &dma_desc = bar0->DMA_DESC[dma_index];
+    volatile dma_descriptor_t &dma_desc = bar0_regs->DMA_DESC[dma_index];
 
     // Address 0x0000 - DMA_DESC_0
-    dma_desc.start_address = start;
-    dma_desc.end_address = start + transfer_size;
+    dma_desc.start_address = interface.iova;
+    dma_desc.end_address = interface.iova + transfer_size;
 
     // Adderss 0x0030 - DMA_DESC_1a
     dma_desc.tlp = MAX_TLP_BYTES / 4;
     dma_desc.read = 0;
     dma_desc.wrap_around = 0;
 
+    interface.dma_map_buffer(buffer);
+
     // Address 0x0400 - DMA_DESC_ENABLE
-    bar0->DMA_DESC_ENABLE |= 0x1 << dma_index;
+    bar0_regs->DMA_DESC_ENABLE |= 0x1 << dma_index;
 }
 
 Wupper::~Wupper() {}
