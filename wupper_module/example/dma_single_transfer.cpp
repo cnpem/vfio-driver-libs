198ed611f02bb9b7a91040682a3f551b7d3ce767
Gustavo Montenari Pechta <mrpecz@proton.me>
2026-07-27 14:48:09 -0300

    wupper_module: Refactor dma_single_transfer application
    Changed the order of the calls to first call dma_to_host() and then read
    BAR0 data. This way we can verify what the Wupper class method wrote in
    the device registers. Changed the program usage to be more flexible
    receiving the device's identifications needed to setup the VFIO
    interface and the size of the transfer via command line instead of hard
    coding them.

--- wupper_module/example/dma_single_transfer.cpp
+++ wupper_module/example/dma_single_transfer.cpp
@@ -8,20 +8,27 @@ int32_t read_region(void *region, uint64_t offset, uint64_t length);
 int main(int argc, char **argv)
 {
     try {
+        if (argc != 4)
+            throw std::runtime_error(
+                "Expected arguments: <Device DBDF> <IOMMU group ID> "
+                "<Transfer size in number of pages>");
+
         // Initialize device interface
-        Wupper xupp3r = Wupper("0000:01:00.0", 2);
+        Wupper xupp3r = Wupper(argv[1], std::stoi(argv[2]));
+
+        // Initialize region to receive data
+        size_t transfer_size = 4096 * std::stoi(argv[3]);
+        DataBuffer buffer = DataBuffer(transfer_size, PROT_READ | PROT_WRITE);
+
+        // Do DMA transfer
+        xupp3r.dma_to_host(transfer_size, DMA_DESC_0, buffer);
 
         // Read BAR0
         // The registers are 16 bytes long, so add 0x10 to the end address
         // to print the content of the register on that address
         read_region((void *)xupp3r.bar0.data, 0, xupp3r.bar0.size + 0x10);
 
-        // Initialize region to receive data
-        size_t transfer_size = 4096 * 10;
-        DataBuffer buffer = DataBuffer(transfer_size, PROT_READ | PROT_WRITE);
-
-        // Do DMA transfer
-        xupp3r.dma_to_host(transfer_size, DMA_DESC_0, buffer);
+        // Read transfered data
         read_region((void *)buffer.data, 0, buffer.size + 0x10);
     } catch (std::runtime_error &e) {
         std::perror(e.what());
