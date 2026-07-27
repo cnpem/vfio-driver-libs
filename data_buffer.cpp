184cd4b47a7e1874f442246d8cc409cfb8160f32
Gustavo Montenari Pechta <mrpecz@proton.me>
2026-07-27 09:04:58 -0300

    vfio: Refactor realloc_buffer()
    This method was updating only the "data" member of the class and leaving
    the other ones unchanged

--- data_buffer.cpp
+++ data_buffer.cpp
@@ -15,16 +15,21 @@ DataBuffer::DataBuffer(size_t size, int prot, int flags, int fd, off_t offset)
 }
 
 void DataBuffer::realloc_buffer(
-    size_t size, int prot, int flags, int fd, off_t offset)
+    size_t n_size, int n_prot, int n_flags, int n_fd, off_t n_offset)
 {
     if (data != nullptr) {
         if (munmap((void *)data, size) == -1)
             throw std::runtime_error("munmap(): Failed to munmap buffer");
     }
 
-    data = mmap(NULL, size, prot, flags, fd, offset);
+    data = mmap(NULL, n_size, n_prot, n_flags, n_fd, n_offset);
     if (data == MAP_FAILED)
         throw std::runtime_error("mmap(): Failed to mmap buffer");
+    size = n_size;
+    prot = n_prot;
+    flags = n_flags;
+    fd = n_fd;
+    offset = n_offset;
 }
 
 DataBuffer::~DataBuffer()
--- data_buffer.h
+++ data_buffer.h
@@ -26,17 +26,17 @@ public:
      * @brief Reallocates the class buffer on the process address space
      * @description Does the same thing as the class constructor, giving the
      * user the possibility to reallocate the buffer with different parameters
-     * @param[in] size Size of the region
-     * @param[in] prot Desired memory protection of the mapping
-     * @param[in] flags Determines wheter updates to the mapping are visible to
+     * @param[in] n_size New size of the region
+     * @param[in] n_prot Desired memory protection of the new mapping
+     * @param[in] n_flags Determines wheter updates to the new mapping are visible to
      * other processes mapping the same region. Defaults to MAP_ANONYMOUS
-     * @param[in] fd File descriptor reference, in case of a file mapping.
+     * @param[in] n_fd New file descriptor reference, in case of a file mapping.
      * Defaults to -1
-     * @param[in] offset Offset in the file, in case of a file a mapping.
+     * @param[in] n_offset New offset in the file, in case of a file a mapping.
      * Defaults to 0
      */
     void realloc_buffer(
-        size_t size, int prot, int flags = MAP_ANONYMOUS, int fd = -1, off_t offset = 0);
+        size_t n_size, int n_prot, int n_flags = MAP_ANONYMOUS, int n_fd = -1, off_t n_offset = 0);
 
     volatile void *data = nullptr;
     size_t size = 0;
